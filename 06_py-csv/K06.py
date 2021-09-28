6b5d6d6b7839fc5f924df64da8a38a4a36a466c5
Yuqing Wu <ywu20@stuy.edu>
2021-09-28 18:29:46 -0400

    put into functions

--- 06_py-csv/K06.py
+++ 06_py-csv/K06.py
@@ -6,20 +6,30 @@
 import csv
 import random
 
+#create a dictionary for the csv file
 occupations = {}
 
+def select_occupations():
+    for key, value in occupations.items():
+        random -= value
+        if(random <=0):
+            print(key)
+            break
 
-with open('occupations.csv') as csv_file:
-    reader = csv.reader(csv_file, delimiter=',')
-    for row in reader:
-        if(row[0] == "Job Class") == False :
-            occupations[row[0]] = float (row[1])
+def main():
+
+    with open('occupations.csv') as csv_file:
+        #reads the csv file and splits by the ','
+        reader = csv.reader(csv_file, delimiter=',')
+
+        #puts the csv file in a dictionary with the string part as the key
+        for row in reader:
+            if(row[0] == "Job Class") == False :
+                occupations[row[0]] = float (row[1])
 
 
-random = random.uniform(0,occupations['Total'])
-del occupations['Total']
-for key, value in occupations.items():
-    random -= value
-    if(random <=0):
-        print(key)
-        break
+    random = random.uniform(0,occupations['Total'])
+    del occupations['Total']
+    select_occupations()
+    
+main()
