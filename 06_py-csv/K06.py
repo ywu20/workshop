0ea747e9af324ec2778a0906451d4a6a8efd6cc0
Yuqing Wu <ywu20@stuy.edu>
2021-09-28 18:10:18 -0400

    open file

---
+++ 06_py-csv/K06.py
@@ -0,0 +1,26 @@
+#Team Polar - Jesse Xie, Rachel Xiao, Yuqing Wu
+#SoftDev
+#K06: StI/O: Divine your Destiny!
+#2021-09-28
+
+import csv
+import random
+
+occupations = {}
+
+
+with open('occupations.csv') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for row in reader:
+        if(row[0] == "Job Class") == False :
+            occupations[row[0]] = float (row[1])
+
+
+random = random.uniform(0.0,occupations['Total'])
+
+del occupations['Total']
+print(occupations.keys())
+
+
+print(occupations)
+print(random)
