6dca7488579c0961a3d539edb7080231e0f2958b
Yuqing Wu <ywu20@stuy.edu>
2021-10-05 13:45:50 -0400

    working code

--- 10_flask/occupy_flask_st/app.py
+++ 10_flask/occupy_flask_st/app.py
@@ -8,9 +8,16 @@ app = Flask(__name__) #create instance of class Flask
 
 @app.route("/")       #assign fxn to route
 def hello_world():
+    keys = ""
     print("the __name__ of this module is... ")
     print(__name__)
-    return K06.main()
+    heading = '''Team Polar: Yuqing Wu, Jesse Xie, Rachel Xiao <br/>
+    SoftDev <br/>
+    K10 -- Putting Little Pieces Together<br/>
+    2021-10-04 <br/><br/>'''
+
+#apparently the problem goes away if we use dictionary.keys() instead of a list
+    return ""+heading+K06.main() + "<br/>\n<br/>"+str(K06.occupations.keys())
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
