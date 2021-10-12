6cd6e4128f6ca5fcddcd2caf1bda4b9b63777d11
Yuqing Wu <ywu20@stuy.edu>
2021-10-12 09:09:41 -0400

    classwork table made

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -6,12 +6,29 @@
 
 
 import random
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__);
 
 @app.route("/")
 def hello_world():
+
+
+    #html = "<h2> Neon Duckies -- Yuqing Wu, Sophie Liu, Hebe Huang </h2><hr>";
+
+    #html += "Random Weighted Job: " + (random.choices(list(dictionary), weights=dictionary.values()))[0] + "<br><br><br>";
+
+    #html += "<table> <tr> <th> Job </th> <th> Percentage </th> </tr>";
+    #for i in list(dictionary):
+    #    html += "<tr>";
+    #    html += ("<td>" + i + "&nbsp;&nbsp;&nbsp;</td>");
+    #    html += ("<td>" + str(dictionary[i]) + "% </td>");
+    #    html += "</tr>";
+    return ""
+
+
+@app.route("/occupyflaskst")
+def test_tmplt():
     file = open("data/occupations.csv");
     lines = file.read().split("\n");
     del lines[0]; #Remove "Job Class, Percentage" line
@@ -27,18 +44,10 @@ def hello_world():
 
     del split[len(split)-1]; # Remove "Total" as a job
     dictionary = dict(split)
+    job = (random.choices(list(dictionary), weights=dictionary.values()))[0]
 
-    html = "<h2> Neon Duckies -- Yuqing Wu, Sophie Liu, Hebe Huang </h2><hr>";
+    return render_template( 'tablified.html', foo="random occupation", random_job = job, collection=dictionary)
 
-    html += "Random Weighted Job: " + (random.choices(list(dictionary), weights=dictionary.values()))[0] + "<br><br><br>";
 
-    html += "<table> <tr> <th> Job </th> <th> Percentage </th> </tr>";
-    for i in list(dictionary):
-        html += "<tr>";
-        html += ("<td>" + i + "&nbsp;&nbsp;&nbsp;</td>");
-        html += ("<td>" + str(dictionary[i]) + "% </td>");
-        html += "</tr>";
-
-    return html;
 app.debug = True;
 app.run()
