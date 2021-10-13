de4fb70f5f59fcad70f2ef1bc4d1474fccc6c5e7
Yuqing Wu <ywu20@stuy.edu>
2021-10-12 21:16:54 -0400

    cleaner way of random job

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -6,47 +6,51 @@
 
 
 import random
+import csv
 from flask import Flask, render_template
 
 app = Flask(__name__);
-
+occupations = {}
 @app.route("/")
 def hello_world():
-
-
-    #html = "<h2> Neon Duckies -- Yuqing Wu, Sophie Liu, Hebe Huang </h2><hr>";
-
-    #html += "Random Weighted Job: " + (random.choices(list(dictionary), weights=dictionary.values()))[0] + "<br><br><br>";
-
-    #html += "<table> <tr> <th> Job </th> <th> Percentage </th> </tr>";
-    #for i in list(dictionary):
-    #    html += "<tr>";
-    #    html += ("<td>" + i + "&nbsp;&nbsp;&nbsp;</td>");
-    #    html += ("<td>" + str(dictionary[i]) + "% </td>");
-    #    html += "</tr>";
     return ""
+
+def select_occupations(rand):
+    for key, value in occupations.items():
+        #As the dictionary is being looped through, each time the random value generated would subtract
+        #the percentage of the occupations. This is kind of like designating areas where the random number
+        #can land on and each area would be represented by an occupation.
+        rand -= value
+
+        #if the random number is less than 0 after the subtraction, that means it's supposed to land within
+        #the area that's represented by this occupation
+        if(rand <=0):
+            return key
 
 
 @app.route("/occupyflaskst")
 def test_tmplt():
-    file = open("data/occupations.csv");
-    lines = file.read().split("\n");
-    del lines[0]; #Remove "Job Class, Percentage" line
-    split = [];
-    for i in lines:
-        if "," in i:
-            #remove quotes, split string into job and %, then convert % to float
-            i = i.replace("\"","");
-            comma = i.rsplit(",",1);
-            comma[1] = float(comma[1]);
-            #add to necessary arrays
-            split.append(comma);
 
-    del split[len(split)-1]; # Remove "Total" as a job
-    dictionary = dict(split)
-    job = (random.choices(list(dictionary), weights=dictionary.values()))[0]
+#choose an occupation with weighted percentage
 
-    return render_template( 'tablified.html', foo="random occupation", random_job = job, collection=dictionary)
+    with open('data/occupations.csv') as csv_file:
+        #reads the csv file and splits by the ','
+        reader = csv.reader(csv_file, delimiter=',')
+        #puts the csv file in a dictionary with the string part as the key and the percent part as value
+        for row in reader:
+            if(row[0] == "Job Class") == False :
+                occupations[row[0]] = float (row[1])
+
+    #Generate a random double from 0 to 99.8
+    rand = random.uniform(0,occupations['Total'])
+
+    #delete 'Total' from dictionary so it will never output 'Total'
+    del occupations['Total']
+
+    #select random job
+    job = select_occupations(rand)
+
+    return render_template( 'tablified.html', foo="random occupation", random_job = job, collection=occupations)
 
 
 app.debug = True;
