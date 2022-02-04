a295e80f363a9d3e20d5e49778ff2a55afa9a557
Yuqing Wu <ywu20@stuy.edu>
2022-02-04 09:09:05 -0500

    added fact

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,6 +1,26 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname fibfac) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;the function named fact (for factorial) is a procedure (lambda) that takes in a parameter
+; if n is less than or equal to 1, the function should return 1
+; if not, the function should return the value of n times the factorial of the preceding number
+
+(define fact
+  (lambda (n)
+  (if (<= n 1)
+      1
+  (* n (fact (- n 1))))
+  )
+  )
+(fact 0)
+(fact 1)
+(fact 2)
+(fact 3)
+(fact 5)
+(fact 10)
+
+
+
 (define fib
   (lambda (n)
     (if (<= n 1)
@@ -10,5 +30,10 @@
   )
 )
 
+(fib 0)
+(fib 1)
+(fib 2)
+(fib 3)
 (fib 4)
-    
+(fib 5)
+(fib 10)
