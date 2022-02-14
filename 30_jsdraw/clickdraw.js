c3286e0faea495449de81ba11a6e8df3fcf6b2c1
Yuqing Wu <ywu20@stuy.edu>
2022-02-14 10:15:53 -0500

    everything wooorks, didn't do the button text change yet but

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -3,20 +3,48 @@ var ctx = c.getContext("2d");
 var mode = "rect";
 
 var toggleMode = (e) => {
-  console.log("toggling...");
+  console.log("toggling..." + mode);
   if (mode == "rect") {
-    return;
+    mode = "circle";
+  }else{
+    mode = "rect";
   }
-  return;
 }
 
 var drawRect = function(e) {
   var mouseX = e.offsetX;
   var mouseY = e.offsetY;
   console.log("mouseClick registered at ", mouseX, mouseY);
-  console.log("offsetX,y ", e.offsetX, e.offsetY);
   ctx.fillStyle = "red";
   ctx.fillRect(mouseX, mouseY, 100, 200);
 }
 
-document.addEventListener('click', drawRect);
+var drawCircle = function(e){
+  var mouseX = e.offsetX;
+  var mouseY = e.offsetY;
+  console.log("mouseClick registered at ", mouseX, mouseY);
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(mouseX, mouseY, 50, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+var draw = function(e){
+  if (mode == "rect"){
+    drawRect(e);
+  }else{
+    drawCircle(e);
+  }
+}
+
+var wipeCanvas = function(){
+  ctx.clearRect(0,0,c.clientHeight,c.clientWidth);
+}
+
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener('click',wipeCanvas);
+
+var bToggler = document.getElementById("buttonToggle");
+bToggler.addEventListener('click', toggleMode);
+
+c.addEventListener('click', draw);
